c5889374b71ebe5964f27fd517b9553a0ad2d005
celil çavuş <celilcavus13@hotmail.com>
2023-02-21 19:27:18 +0300

    sirket veritabında birim tablosu oluşturuldu

---
+++ sirketVeritabani.sql
@@ -0,0 +1,12 @@
+﻿create database SirketVeritabani
+go
+use SirketVeritabani
+
+create table BIRIM(
+ID INT IDENTITY(1,1) PRIMARY KEY,
+BIRIM_AD NVARCHAR(50)
+)
+GO
+
+
+INSERT INTO BIRIM (BIRIM_AD) VALUES ('KALİTE'),('İDARİ'),('ARGE'),('TEKNİK');
